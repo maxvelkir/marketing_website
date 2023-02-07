9499195a9d97528cdac9a58d34fe97332fc38c41
Maxime Kiriakov <kiriakovmaxim@gmail.com>
2023-02-07 08:40:09 +0200

    release 1.3.5

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MK Consulting</title>
+        <title>Marketing Freaks</title>
         <meta name="description" content="Personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
