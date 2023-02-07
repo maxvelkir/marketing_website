44b07e860d1c14077a8e1e597950a0d661cb9d1e
Maxime Kiriakov <kiriakovmaxim@gmail.com>
2023-02-07 14:18:27 +0200

    add remote Terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "https://nyc3.digitaloceanspaces.com"
+    region                      = "us-east-1"
+    bucket                      = "remote-terraform-state"
+    key                         = "infrastructure/terraform.tfstate"
+  }
 }
 
 variable "do_token" {}
